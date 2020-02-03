19d1f8d7410bae7e341fd38e007b3c866d2bbbf7
Xandowski <xandy-moraes@hotmail.com>
2020-02-02 23:19:21 -0300

    Fibonacci Sequence Challenge

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciSequence
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("The first 20 numbers in Fibonnaci Sequence");
+      var fibonacciSequence = new List<int> { 1, 1 };
+
+      while (fibonacciSequence.Count < 20)
+      {
+        var previous = fibonacciSequence[fibonacciSequence.Count - 1];
+        var previous2 = fibonacciSequence[fibonacciSequence.Count - 2];
+        fibonacciSequence.Add(previous + previous2);
+      }
+
+      foreach (var item in fibonacciSequence)
+        Console.WriteLine(item);
+
+      Console.WriteLine("What position do you want to know the value?");
+      string userNumber = Console.ReadLine();
+      int number = int.Parse(userNumber);
+      double fibo2 = (Math.Pow((1 + Math.Sqrt(5)) / 2, number) - Math.Pow((1 - Math.Sqrt(5)) / 2, number)) / Math.Sqrt(5);
+
+      Console.WriteLine(Math.Round(fibo2));
+    }
+  }
+}
