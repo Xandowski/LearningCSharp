5a9b7d9d4ba3b1bfbb21c400e10af2fe05eaddb1
Xandowski <xandy-moraes@hotmail.com>
2020-02-06 14:43:14 -0300

    Pyramid

---
+++ Pyramid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Pyramid
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Digite a number: ");
+      int number = Int32.Parse(Console.ReadLine());
+      DrawPyramid(number);
+    }
+
+    private static void DrawPyramid(int n)
+    {
+      for (int i = 0, j = n; i < n; i++, j--)
+      {
+        int space = n + (n - 1);
+        for (int k = 0; k < space; k++)
+        {
+          if (k < j - 1)
+          {
+            Console.Write(" ");
+            space--;
+          }
+          else
+          {
+            Console.Write("*");
+          }
+
+        }
+
+        Console.WriteLine();
+      }
+    }
+  }
+}
